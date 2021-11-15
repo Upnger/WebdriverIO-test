c9cf17853c5f02cf746faabdb89f65a699b43138
Robin Persson <robin.persson@solitio.se>
2021-11-16 00:28:05 +0100

    test a text based on language

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,6 +3,14 @@ describe('topNavCarMenu', () => {
         await browser.url(`/`);
 
         await expect($('#nav\\:topNavCarMenu')).toBeExisting();
+        const lang = await browser.execute(()=>document.documentElement.lang)
+
+        // we assume that there is only a swedish and english variant of the page. Probably not true but this is not a real test suit.
+        if(lang === 'sv' ){
+            await expect($('#nav\\:topNavCarMenu')).toHaveTextContaining('Våra bilar');
+        }else{
+            await expect($('#nav\\:topNavCarMenu')).toHaveTextContaining('Our Cars');
+        }
     });
 });
 
